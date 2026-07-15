75cd126b7cdd7a651d58b307587a841ace962db2
ajirang1 <hehasfelt@gmail.com>
2026-07-15 09:14:47 +0900

    feat: change config

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 type Heartbeat = {
   deviceName: string;
--- web/next.config.ts
+++ web/next.config.ts
@@ -1,11 +1,13 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
+  allowedDevOrigins: ['100.83.1.6'],
+
   async rewrites() {
     return [
       {
         source: '/backend/:path*',
-        destination: 'http://127.0.0.1:3000/api/:path*',
+        destination: 'http://127.0.0.1:4000/api/:path*',
       },
     ]
   },
