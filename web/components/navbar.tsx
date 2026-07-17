69444dd7f4fdb3e45309d3429ef4bcffcb0dec80
ajirang1 <hehasfelt@gmail.com>
2026-07-18 00:48:06 +0900

    add externallink to navbar

--- web/components/navbar.tsx
+++ web/components/navbar.tsx
@@ -6,45 +6,47 @@ import { ExternalLink } from 'lucide-react';
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
   return (
-<nav className="bg-gray-900 text-white sticky top-0 z-50">
-  <div className="max-w-7xl mx-auto px-4 flex justify-between h-16 items-center">
-    <div className="flex items-baseline gap-2">
-      <Link
-        href="https://nexus.yuktn.dev"
-        className="font-bold text-3xl"
-      >
-        nexus
-      </Link>
+    <nav className="bg-gray-900 text-white sticky top-0 z-50">
+      <div className="max-w-7xl mx-auto px-4 flex justify-between h-16 items-center">
+        <div className="flex items-baseline gap-2">
+          <Link
+            href="https://nexus.yuktn.dev"
+            className="font-bold text-3xl"
+          >
+            nexus
+          </Link>
 
-      <Link
-        href="https://yuktn.dev"
-        className="text-base text-gray-400"
-      >
-        by yuktn
-      </Link>
-    </div>
+          <Link
+            href="https://yuktn.dev"
+            className="text-base text-gray-400 flex"
+          >
+            by yuktn
+            <ExternalLink size={10} color="#9CA3AF" className="mt-2 ml-2" />
+          </Link>
 
-    <div className="hidden md:flex space-x-4">
-      <Link
-        href="https://github.com/yuktn/nexus"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="flex items-center gap-1"
-      >
-        yuktn/nexus
-        <ExternalLink size={16} />
-      </Link>
-    </div>
+        </div>
 
-    <button
-      className="md:hidden"
-      onClick={() => setIsOpen(!isOpen)}
-    >
-      Menu
-    </button>
-  </div>
+        <div className="hidden md:flex space-x-4">
+          <Link
+            href="https://github.com/yuktn/nexus"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex items-center gap-1"
+          >
+            yuktn/nexus
+            <ExternalLink size={16} />
+          </Link>
+        </div>
 
-  {isOpen && <div className="md:hidden">...Links...</div>}
-</nav>
+        <button
+          className="md:hidden"
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          Menu
+        </button>
+      </div>
+
+      {isOpen && <div className="md:hidden">...Links...</div>}
+    </nav>
   );
 }
